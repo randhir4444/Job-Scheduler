8b6a99420fca5bbf47abc955bee418f32b60f556
Randhir singh <randhir4444@gmail.com>
2021-09-12 02:55:53 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,90 @@
+.main-container{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+}
+.server{
+    width: 40%;
+    height: 100vh;
+    border-right: 1px solid black;
+    display: flex;
+    flex-direction: column;
+}
+.serverlist{
+    height: 70px;
+    width: 450px;
+    border-radius: 5px;
+    border: 2px solid black;
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.action{
+    width: 30%;
+    height: 100vh;
+    
+    padding:20px
+}
+.action_list{
+    width: 100%;
+}
+.waitingTask{
+    width: 300px;
+    margin: 10px;
+}
+.assignment{
+    width: 30%;
+    height: 100vh;
+    border-left: 1px solid black;
+    padding: 10px;
+}
+.serverButton{
+    width: 100%;
+    display: flex;
+}
+.serverButton button{
+    margin: 10px;
+    background-color:purple;
+    color: white;
+    height:40px;
+    border-radius: 5px;
+    width: 150px;
+}
+#addTask{
+    background-color:purple;
+    color: white;
+    height:40px;
+    border-radius: 5px;
+    width: 150px;
+}
+#inputvalue{
+    height:35px;
+    margin:10px;
+    width:150px
+}
+.pb_container{
+    height: 20px;
+    border-radius: 7px;
+    border: solid purple;
+    width: 350px;
+    position: relative;
+    font-size:13px;
+}
+span{
+    line-height: 20px;
+}
+.pb_container .progress-bar{
+    position: absolute;
+    height: 100%;
+    background-color:purple;
+    
+}
+#play-animation{
+    animation:progress-animation 5s forwards;
+}
+@keyframes progress-animation {
+    0% {width:0%;}
+    100%{width:100%;}
+}
